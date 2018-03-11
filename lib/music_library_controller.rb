d318a6695dadb2198d12d3457257b77cb88d6790
Zac Willmington <zac.willmington@gmail.com>
2018-03-11 00:05:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -110,10 +110,11 @@ class MusicLibraryController
         number = input.to_i
           #binding.pry
          if number > 0 && number <= self.list_songs.length
-             
+
            self.list_songs.to_a[input + 1].split(" - ").each do |song|
               puts "Playing #{song[0]} by #{song[1]}"
             end
+            
         end
     end
 end
